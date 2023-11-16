014b01290944bdb1d09ae515b1cfda4ec79cc72f
sajusun <sajuislam266@gmail.com>
2023-11-16 15:40:25 +0600

    with basic featured

--- lib/main.dart
+++ lib/main.dart
@@ -31,27 +31,65 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
    List<Map<String,dynamic>> items=[];
+   bool isSearch=false;
+   TextEditingController searchText=TextEditingController();
    @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
   }
   void getNotes() async{
         items =await MyDb.getAllNote();
+  }
+  Widget searchWidget(bool search){
+     if(search){
+       return TextFormField(
+         onTapOutside: (pointer){
+           FocusManager.instance.primaryFocus?.unfocus();
+         },
+         textInputAction: TextInputAction.search,
+         controller: searchText,
+         maxLines: 1,
+         decoration: InputDecoration(hintText: "Search", border: InputBorder.none),
+         scrollPadding: EdgeInsets.all(20.0),
+         keyboardType: TextInputType.text,
+         autofocus: true,
+         onChanged: (value){
+           searchData(searchText.text);
+         },
+       );
+     }else{
+       return const Text("NoteBook");
+     }
+  }
+
+  Future<void> searchData(searchText) async {
+     var result = await MyDb.searchNote(searchText);
+     items=result;
   }
 
   @override
   Widget build(BuildContext context) {
     Future.delayed(const Duration(milliseconds: 10),() {
       setState(() {
-        getNotes();
+        if(isSearch) {
+          searchData(searchText.text);
+        }else{
+          getNotes();
+        }
+
       });
     },);
 
-print(currentDateTime());
     return Scaffold(
-      appBar: AppBar(title:const Text("NoteBook"),centerTitle: true,
+      appBar: AppBar(leading: IconButton(onPressed: (){
+        isSearch=!isSearch;
+        if(!isSearch){
+          searchText.text="";
+        }
+
+      }, icon: Icon(Icons.search_rounded)),
+        title:searchWidget(isSearch),centerTitle: true,
         actions: [IconButton(icon:const Icon(Icons.add_box_outlined,size: 24,),
         onPressed: () {
         Navigator.push(context, MaterialPageRoute(builder: (context)=>AddNote())) ;
@@ -78,6 +116,7 @@ print(currentDateTime());
                     subtitle: Text(items[index]['creationTime']),
                     trailing: IconButton(onPressed: ()  {
                       deleteDialogBox(context, items[index]['id']);
+
                     },icon:const Icon(Icons.delete),color: Colors.redAccent,),
 
                   ),
